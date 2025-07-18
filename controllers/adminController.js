342271107bbc137751411a99b17a0cba5cfd211b
Jay Jain <jayjitendrajainn@gmail.com>
2025-07-18 20:09:00 +0530

    17th commit

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -59,16 +59,76 @@ exports.admin_products_view = async(req,res,next) =>{
     }
 }
 
-// exports.admin_product_update = async(req,res,next) =>{
-//     try{
-//         const {product_id} = req.params;
-//         const {product_name,product_price,product_image} = req.body;
-//         await sql`
-//         UPDATE products_data
-//         set`
+exports.admin_product_update = async (req, res, next) => {
+  try {
+    const { product_id } = req.params;
+    const {
+      product_name,
+      product_price,
+      product_image,
+      product_discount,
+      product_max_price,
+      users_tracked,
+      product_url
+    } = req.body;
 
-//     }
-//     catch(error){
-//         return res.status(500).json({ message: "Server error" });
-//     }
-// }
+    await sql`
+      UPDATE products_data
+      SET 
+        product_name = ${product_name},
+        product_price = ${product_price},
+        product_image = ${product_image},
+        product_discount = ${product_discount},
+        product_max_price = ${product_max_price},
+        users_tracked = ${users_tracked},
+        product_url = ${product_url}
+      WHERE product_id = ${product_id}
+    `;
+
+    return res.json({ message: "success" });
+  } catch (error) {
+    console.error("Admin product update error:", error);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.admin_product_delete = async(req,res,next) =>{
+    try{
+        const {product_id} = req.params
+        await sql`BEGIN`;
+            await sql`
+                UPDATE signup
+                SET products_tracking = products_tracking - 1
+                WHERE email IN (
+                    SELECT email FROM user_urls WHERE product_id = ${product_id})
+            `;
+            await sql`
+                DELETE 
+                FROM products_data
+                WHERE product_id = ${product_id}
+            `;
+            await sql`
+                DELETE
+                FROM user_urls
+                WHERE product_id = ${product_id}
+            `;
+            await sql`
+                DELETE
+                FROM product_price_history
+                WHERE product_id = ${product_id}
+            `;
+            await sql`
+                DELETE
+                FROM product_ids
+                WHERE product_id = ${product_id}
+            `;
+        await sql`COMMIT`;
+        
+        return res.json({ status: "success", message: "Product deleted successfully"});
+    }
+    catch(error){
+        await sql`ROLLBACK`;
+        return res.status(500).json({ message: "Server error" });
+    }
+}
+
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -3,8 +3,10 @@ const adminController = require('../controllers/adminController');
 adminRouter = express.Router();
 
 adminRouter.get('/admindashboard',adminController.admindashboard);
-adminRouter.get('/allusersdetails',adminController.allusersdetails)
-adminRouter.get('/admin_products_view',adminController.admin_products_view)
-// adminRouter.put('/admin_product_update:product_id',adminController.admin_product_update)
+adminRouter.get('/allusersdetails',adminController.allusersdetails);
+adminRouter.get('/admin_products_view',adminController.admin_products_view);
+adminRouter.put('/admin_product_update/:product_id',adminController.admin_product_update);
+adminRouter.delete('/admin_product_delete/:product_id',adminController.admin_product_delete);
+
 
 module.exports = adminRouter;
